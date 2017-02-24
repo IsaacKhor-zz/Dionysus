d9bde3c384699268fc8a2163e362438fde5da2fd
Isaac Khor <services@isaackhor.com>
2017-02-24 09:17:42 +0800

    Initial Commit

---
+++ Dionysus/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Dionysus
+//
+//  Created by Clarence Rosary Clark on 24/02/2017.
+//  Copyright © 2017 Isaac Khor Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
